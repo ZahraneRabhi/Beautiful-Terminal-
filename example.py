fb7ca8b7a532300505142625b4193f0f563e0868
Zahrane <zahranerabhi@outlook.com>
2024-05-06 14:34:03 +0200

    Code Refactor

--- example.py
+++ example.py
@@ -3,6 +3,10 @@ from utils import *
 #change_terminal_background(color="BLUE")
 #loading_bar("FAST", 100, "TERMINAL")
 
-message(message="Facebook API DATA👍", color="BLUE")
-message(message="Twitter API DATA", color="BLUE")
-message(message="Google API DATA", color="RED")
+# message(message="Facebook API DATA👍", color="BLUE")
+# message(message="Twitter API DATA", color="BLUE")
+# message(message="Google API DATA", color="RED")
+
+dic = {"1": 4}
+
+print(str(dic))
